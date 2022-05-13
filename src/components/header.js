47e1901cf5385bb0c54577a2720af3500579c2ab
EletherioAlexandre <eletherioalexandre@gmail.com>
2022-05-13 00:35:41 -0300

    Project started

---
+++ src/components/header.js
@@ -0,0 +1,24 @@
+import "../pages/Main/styles.scss";
+import logo from "../assets/images/logo.svg";
+function Header() {
+  return (
+    <>
+      <header>
+        <div className="top-from-header">
+          <img src={logo} alt="SmartFit" />
+        </div>
+        <div className="title-from-header">
+          <h1>REABERTURA SMART FIT</h1>
+          <div className="border-bottom"></div>
+          <p>
+            O horário de funcionamento das nossas unidades está seguindo os
+            decretos de cada município. Por isso, confira aqui se a sua unidade
+            está aberta e as medidas de segurança que estamos seguindo.
+          </p>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import "./styles.scss";
+import Header from "../../components/header.js";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
